af420cf6e2f9c4d812527c1825846a1264de8c37
amilakanishka <amilakanishka@gmail.com>
2020-08-30 04:34:03 +1000

    initial commit

---
+++ static/js/bonus.js
@@ -0,0 +1,8 @@
+//d3.json("samples.json").then((importedData) => {
+   // var data = importedData.samples;
+  
+    // Sort the data array using the greekSearchResults value
+    // data.sort(function(a, b) {
+   //    console.log(importedData);
+  //   return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
+//});
